ed883ad63c0d85c9f9491bccefe93a1d1ca89d34
jakob <jak.gerhardt@gmail.com>
2023-10-03 20:49:07 +0200

    chore: improve template

--- nix/nix2dream/renderers/_template/service.nix
+++ nix/nix2dream/renderers/_template/service.nix
@@ -9,4 +9,15 @@
   t = l.types;
   pkgs = specialArgs.nixpkgs;
 in {
+  #  options.service-managers."NAME" = {
+  #    optionName = l.mkOption {
+  #      type = t.oneOf [t.str t.package];
+  #      description = ''
+  #        the launchCommand in the procfile:
+  #
+  #        <defaultServiceName>: <launchCommand>
+  #      '';
+  #      default = config.service.entrypoint;
+  #    };
+  #  };
 }
